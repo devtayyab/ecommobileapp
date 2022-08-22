76f380603ec91ef95cf6bdcf38cceef783d1f017
devtayyab <gujjartayyab9494@gmail.com>
2022-08-22 22:36:14 +0500

    Dytnamic Checkout

--- app/components/SearchBox.js
+++ app/components/SearchBox.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {View, Pressable, TextInput} from 'react-native';
-import {scale} from 'react-native-size-matters';
-import {appColors} from '../utils/appColors';
+import { View, Pressable, TextInput } from 'react-native';
+import { scale } from 'react-native-size-matters';
+import { appColors } from '../utils/appColors';
 import Feather from 'react-native-vector-icons/Feather';
-export default function SearchBox({autoFocus,onFoucs, hideCamra, onRightIconPress,rightIcon}) {
+export default function SearchBox({ autoFocus, onFoucs, hideCamra, onRightIconPress, rightIcon }) {
   return (
-    <View style={{justifyContent: 'space-between', flexDirection: 'row', alignItems:'center',}}>
+    <View style={{ justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', }}>
       <View
         style={{
-          flex:1,
+          flex: 1,
           paddingHorizontal: scale(20),
           borderRadius: scale(20),
           alignItems: 'center',
@@ -18,15 +18,15 @@ export default function SearchBox({autoFocus,onFoucs, hideCamra, onRightIconPres
           height: scale(40),
         }}>
         <Feather name="search" size={scale(20)} color={appColors.black} />
-        <TextInput 
-        autoFocus={autoFocus}
+        <TextInput
+          autoFocus={autoFocus}
           onFocus={onFoucs && onFoucs}
-          style={{flex: 1, paddingLeft: scale(10)}}
+          style={{ flex: 1, paddingLeft: scale(10) }}
         />
       </View>
-      {!hideCamra && (
+      {hideCamra && (
         <Pressable
-          onPress={onRightIconPress&&onRightIconPress}
+          onPress={onRightIconPress && onRightIconPress}
           style={{
             borderRadius: scale(20),
             width: scale(40),
@@ -34,9 +34,9 @@ export default function SearchBox({autoFocus,onFoucs, hideCamra, onRightIconPres
             backgroundColor: appColors.primary,
             justifyContent: 'center',
             alignItems: 'center',
-            marginLeft:scale(20)
+            marginLeft: scale(20)
           }}>
-          <Feather name={rightIcon ? rightIcon :"camera"} size={scale(18)} color={appColors.white} />
+          {/* <Feather name={rightIcon ? rightIcon :"camera"} size={scale(18)} color={appColors.white} /> */}
         </Pressable>
       )}
     </View>
--- app/redux/reducer/cart.js
+++ app/redux/reducer/cart.js
@@ -1,30 +1,44 @@
-import { ADD_TO_CART,REMOVE_FROM_CART } from "../cartAction"; //action
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../cartAction"; //action
 
 const intiialState = {
-    cartItems :[] // multiple
+    cartItems: [] // multiple
 }
 
 
-export default function (state =intiialState,action) {
-const {type, payload} =action
+export default function (state = intiialState, action) {
+    const { type, payload } = action
     switch (type) {
-        case ADD_TO_CART: 
-       return  {
-            ...state,
-            cartItems : [...state.cartItems, payload]
-        }
-    case REMOVE_FROM_CART:
-        const itemsLeft = state.cartItems?.filter((item,index)=>{
-              if(item?.name !=  payload)
-              return item
-        })
-        console.log({itemsLeft});
-        return {
-            ...state,
-            cartItems : [...itemsLeft]
-        }
+        case ADD_TO_CART:
+            console.log(payload)
+            const findId = state.cartItems.find((item) => item?.id == payload?.id)
+            if (findId) {
+                console.log(findId)
+                const filter = state.cartItems.filter((item) => item.id !== payload.id)
+
+                return {
+                    ...state,
+                    cartItems: [...filter, { ...findId, itemQuantity: findId.itemQuantity + 1 }]
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, { ...payload, itemQuantity: 1 }]
+                }
+            }
+        case REMOVE_FROM_CART:
+            const itemsLeft = state.cartItems?.filter((item, index) => {
+                console.log(payload)
+                if (item?.id != payload)
+
+                    return { ...item, itemQuantity: 1 }
+            })
+            console.log({ itemsLeft });
+            return {
+                ...state,
+                cartItems: [...itemsLeft]
+            }
         default:
             return state
     }
-    
+
 } 
